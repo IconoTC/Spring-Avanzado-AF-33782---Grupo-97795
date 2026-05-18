56f345d8526509a7f73f93fb645962f1cfc6dd03
Javier <35864946+jmagit@users.noreply.github.com>
2026-05-18 11:11:07 +0200

    nut null

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -1,13 +1,24 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.nulabilidad.Dummy;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.err.println("Aplicacion arrancada...");
+		var dummy = new Dummy();
+		String cad = "";
+		dummy.setCadena(cad);
 	}
 
 }
