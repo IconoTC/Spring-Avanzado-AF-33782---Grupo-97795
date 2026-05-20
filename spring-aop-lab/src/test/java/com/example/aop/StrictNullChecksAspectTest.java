c8e52c748f715b9fe67aac1d0df0e0f41179f6e9
Javier <35864946+jmagit@users.noreply.github.com>
2026-05-20 09:06:22 +0200

    bug @EnableAspectJAutoProxy

--- spring-aop-lab/src/test/java/com/example/aop/StrictNullChecksAspectTest.java
+++ spring-aop-lab/src/test/java/com/example/aop/StrictNullChecksAspectTest.java
@@ -14,10 +14,12 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @Import(DummyService.class)
+@EnableAspectJAutoProxy(exposeProxy = true)
 class StrictNullChecksAspectTest {
     @Autowired
     DummyService service;
