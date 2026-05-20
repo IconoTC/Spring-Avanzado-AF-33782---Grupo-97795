9beba1e736758ef19d7ceed41201c66759d46c92
Javier <35864946+jmagit@users.noreply.github.com>
2026-05-20 10:23:48 +0200

    consultas

--- demo-jdbc/src/main/java/com/example/DemoJdbcApplication.java
+++ demo-jdbc/src/main/java/com/example/DemoJdbcApplication.java
@@ -1,13 +1,47 @@
 package com.example;
 
+import java.time.LocalDateTime;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.simple.JdbcClient;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @SpringBootApplication
 public class DemoJdbcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJdbcApplication.class, args);
+	}
+
+	@Data
+	@NoArgsConstructor
+	@AllArgsConstructor
+	static class Actor {
+		private int actorId;
+		private String firstName;
+		private String lastName;
+		private LocalDateTime lastUpdate;
+	}
+
+	@Bean
+	CommandLineRunner consultas(JdbcClient jdbcClient) {
+		return arg -> {
+			IO.println(jdbcClient.sql("select count(1) from actor").query(Integer.class).single());
+
+			var actor = jdbcClient.sql("select * from actor where actor_id = ?").param(1).query(Actor.class)
+					.optional();
+			if (actor.isPresent())
+				IO.println(actor.get());
+			else {
+				System.err.println("No encontrado");
+			}
+		};
 	}
 
 }
