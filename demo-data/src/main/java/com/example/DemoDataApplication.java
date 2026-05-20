09ebecdf5b23e1d2eed143ae174edee14dc04d07
Javier <35864946+jmagit@users.noreply.github.com>
2026-05-20 12:16:37 +0200

    Spring Data

--- demo-data/src/main/java/com/example/DemoDataApplication.java
+++ demo-data/src/main/java/com/example/DemoDataApplication.java
@@ -1,7 +1,9 @@
 package com.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoDataApplication {
@@ -9,5 +11,14 @@ public class DemoDataApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoDataApplication.class, args);
 	}
+	
+	@Bean
+	CommandLineRunner ejempos(EjemplosDatos demos) {
+		return arg -> {
+//			demos.consultas();
+			demos.actores();
+		};
+	}
+
 
 }
---
+++ demo-data/src/main/java/com/example/EjemplosDatos.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.contracts.domain.repositories.ActoresRepository;
+import com.example.domain.entities.Actor;
+
+@Component
+public class EjemplosDatos {
+
+	@Autowired
+	ActoresRepository daoActors;
+	
+	public void consultas() {
+		daoActors.findAll().forEach(IO::println);
+	}
+	
+	public void actores() {
+		System.out.println(">>> Create");
+		var id = daoActors.save(new Actor("Pepito", "Grillo")).getActorId();
+		daoActors.findAll().forEach(System.out::println);
+		var item = daoActors.findById(id);
+		if (item.isEmpty()) {
+			System.err.println("No encontrado");
+		} else {
+			var actor = item.get();
+			System.out.println("Leido: " + actor);
+			actor.setFirstName(actor.getFirstName().toUpperCase());
+			daoActors.save(actor);
+		}
+		System.out.println(">>> Update");
+		daoActors.findAll().forEach(System.out::println);
+		daoActors.deleteById(id);
+		System.out.println(">>> Delete");
+		daoActors.findAll().forEach(System.out::println);
+
+	}
+
+
+}
